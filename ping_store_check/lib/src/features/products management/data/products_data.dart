67bc26a2ff5862b771ce89d66a4e4bc2a73f62be
AY02 <alessioyang.02@gmail.com>
2023-01-20 14:00:37 +0100

    Big changes

--- ping_store_check/lib/src/features/products management/data/products_data.dart
+++ ping_store_check/lib/src/features/products management/data/products_data.dart
@@ -1,3 +1,18 @@
+/*
+ 0 Barcode
+ 1 Descrizione
+ 2 Quantità
+ 3 Reparto
+ 4 Scorta
+ 5 Fornitore
+ 6 Costo
+ 7 Prezzo 1
+ 8 Prezzo 2
+ 9 Prezzo 3
+10 Note
+11 Immagini
+*/
+
 const List<String> fields = [
   'Barcode', 'Descrizione', 
   'Quantità', 'Reparto', 
--- ping_store_check/lib/src/features/products management/presentation/components/record_form.dart
+++ ping_store_check/lib/src/features/products management/presentation/components/record_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ping_store_check/src/features/products%20management/application/on_pressed_functions.dart';
 import 'package:ping_store_check/src/features/products%20management/data/products_data.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MyRecordForm extends StatefulWidget {
   final List<String> record;
@@ -15,6 +16,8 @@ class MyRecordForm extends StatefulWidget {
 }
 
 class _MyRecordFormState extends State<MyRecordForm> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   
   final List<TextEditingController> _controllers = List.generate(
     fields.length,
@@ -22,16 +25,57 @@ class _MyRecordFormState extends State<MyRecordForm> {
   );
 
   TextFormField _field(int i) {
-    _controllers[i].text = widget.record[i];
+    if(widget.mode == editMode || i == 0) _controllers[i].text = widget.record[i];
     return TextFormField(
       controller: _controllers[i],
       decoration: InputDecoration(hintText: fields[i]),
+      validator: (i==0 || i==7) ? (value) {
+        if(i == 0) {
+          RegExp barcodeValidator = RegExp(r'^\d{13}$');
+          if(!barcodeValidator.hasMatch(value!)) return 'Invalid Barcode';
+        } else {
+          RegExp priceValidator = RegExp(r'^\d+.\d{2}$');
+          if(!priceValidator.hasMatch(value!)) return 'Invalid Price';
+        }
+        return null;
+      } : null,
     );
+  }
+
+  Future<void> _loadRecordData() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      for(int i=0; i<fields.length; i++) {
+        if([2,4,5,6,7].contains(i)) {
+          _controllers[i].text = prefs.getString(fields[i]) ?? '';
+        }
+      }
+    });
+  }
+
+  Future<void> _saveRecordData() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    for(int i=0; i<fields.length; i++) {
+      if([2,4,5,6,7].contains(i)) {
+        await prefs.setString(fields[i], _controllers[i].text);
+      }
+    }
+  }
+
+  Future<void> _clearRecordData() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    for(int i=0; i<fields.length; i++) {
+      if([2,4,5,6,7].contains(i)) {
+        await prefs.remove(fields[i]);
+      }
+      _controllers[i].text = '';
+    }
   }
 
   @override
   void initState() {
     super.initState();
+    if(widget.mode == addMode) _loadRecordData();
   }
 
   @override
@@ -46,14 +90,25 @@ class _MyRecordFormState extends State<MyRecordForm> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       physics: const BouncingScrollPhysics(),
-      child: Column(
-        children: <Widget>[
-          for(int i=0; i<fields.length; i++) _field(i),
-          ElevatedButton(
-            onPressed: () async => await onSendRecord(context, widget.mode, _controllers),
-            child: Text(widget.mode == addMode ? 'Add' : 'Edit'),
-          ),
-        ],
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            for(int i=0; i<fields.length; i++) _field(i),
+            ElevatedButton(
+              onPressed: () async {
+                if(!_formKey.currentState!.validate()) return;
+                await onSendRecord(context, widget.mode, _controllers);
+                if(widget.mode == addMode) await _saveRecordData();
+              },
+              child: Text(widget.mode == addMode ? 'Add' : 'Edit'),
+            ),
+            if(widget.mode == addMode) ElevatedButton(
+              onPressed: () async => await _clearRecordData(),
+              child: const Text('Clear'),
+            ),
+          ],
+        ),
       ),
     );
   }
